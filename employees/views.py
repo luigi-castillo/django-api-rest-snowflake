d882a0a83e6436d6d957bc52191cd8b7d1ddb8c7
Luigi <luigi.castillo@unmsm.edu.pe>
2023-03-10 17:47:43 -0500

    feat: Add bulk upload file feature for department

--- employees/views.py
+++ employees/views.py
@@ -22,18 +22,51 @@ class DepartmentsAPIView(views.APIView):
         serializer = DepartmentsSerializer(depts, many=True)
         return Response(serializer.data)
 
+    def post(self, request):
+        serializer = DepartmentsSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+def decode_csv(file_string, field_name):
+    rows = []
+    for row in file_string.split('\r\r\n'):
+        fields = row.split(',')
+        diction = {}
+        for i in range(len(field_name)):
+            diction[field_name[i]] = fields[i]
+        rows.append(diction)
+    return rows
+
 
 class UploadAPIView(views.APIView):
+    def is_all_rows_valid(self, serializer, rows):
+        all_valid = True
+        for dictionary in rows:
+            serializer = serializer(data=dictionary)
+            if serializer.is_valid():
+                all_valid = True
+            else:
+                all_valid = False
+                break
+        return all_valid
+
+    def insert(self, serializer, rows):
+        for dictionary in rows:
+            serializer = serializer(data=dictionary)
+            if serializer.is_valid():
+                serializer.save()
+
     def post(self, request):
         file_uploaded = request.FILES.get('file_uploaded')
-        file_name = file_uploaded.name
-        file_data = []
-        for row in file_uploaded.read().decode().split('\r\r\n'):
-            file_data.append(row)
-        response = file_data
-        return Response(response)
-        # serializer = DepartmentsSerializer(data=request.data)
-        # if serializer.is_valid():
-        #     serializer.save()
-        #     return Response(serializer.data, status=status.HTTP_201_CREATED)
-        # return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        fields_name = ['id', 'department']
+        rows_in_dict = decode_csv(file_uploaded.read().decode(), fields_name)
+        are_valid = self.is_all_rows_valid(DepartmentsSerializer, rows_in_dict)
+
+        if are_valid:
+            self.insert(DepartmentsSerializer, rows_in_dict)
+            return Response("OK", status=status.HTTP_201_CREATED)
+        else:
+            return Response("error", status=status.HTTP_400_BAD_REQUEST)
