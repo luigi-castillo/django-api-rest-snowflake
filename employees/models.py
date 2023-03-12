2be6e18b6b19a6cb2a4dd27a468114683c6969d9
Luigi <luigi.castillo@unmsm.edu.pe>
2023-03-12 17:28:23 -0500

    feat: Add rejected tables

--- employees/models.py
+++ employees/models.py
@@ -4,17 +4,36 @@ from django.db import models
 # Create your models here.
 class Departments(models.Model):
     id = models.IntegerField(primary_key=True)
-    department = models.TextField(null=True)
+    department = models.TextField()
 
 
 class Jobs(models.Model):
     id = models.IntegerField(primary_key=True)
-    job = models.TextField(null=True)
+    job = models.TextField()
 
 
 class HiredEmployees(models.Model):
     id = models.IntegerField(primary_key=True)
-    name = models.TextField(null=True)
-    datetime = models.TextField(null=True)
+    name = models.TextField()
+    datetime = models.TextField()
     department_id = models.ForeignKey(Departments, on_delete=models.CASCADE)
     job_id = models.ForeignKey(Jobs, on_delete=models.CASCADE)
+
+
+class DepartmentsRejected(models.Model):
+    department_id = models.IntegerField()
+    department = models.TextField()
+
+
+class JobsRejected(models.Model):
+    job_id = models.IntegerField()
+    job = models.TextField()
+
+
+class HiredEmployeesRejected(models.Model):
+    hired_employee_id = models.IntegerField()
+    name = models.TextField()
+    datetime = models.TextField()
+    department_id = models.IntegerField()
+    job_id = models.IntegerField()
+
